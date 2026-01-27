c4b1052e90da27cbd41926c4dfe6c47a4487c3c1
Saimese <JettCG08@gmail.com>
2026-01-27 10:31:06 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://saimese.github.io',
+  site: 'https://Saimese.github.io',
   base: '/saimese-cybersecurity-network',
   integrations: [tailwind()],
   markdown: {
